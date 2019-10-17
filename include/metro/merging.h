3eea67b85ef6a68c1a0868e3e754388f85784c07
SiliconSloth <siliconsloth64@gmail.com>
2019-10-17 17:48:03 +0100

    Added absorb and resolve functions

--- include/metro/merging.h
+++ include/metro/merging.h
@@ -4,5 +4,13 @@ namespace metro {
     string get_merge_message(const Repository& repo);
     void set_merge_message(const Repository& repo, const string& message);
 
+    // Get the commit ID of the merge head. Assumes a merge is ongoing.
+    string merge_head_id(const Repository& repo);
+
     void start_merge(const Repository& repo, const string& sourceName);
+
+    // Create a commit of the ongoing merge and clear the merge state and conflicts from the repo.
+    void resolve(const Repository& repo);
+
+    bool absorb(const Repository& repo, const string& mergeHead);
 }
