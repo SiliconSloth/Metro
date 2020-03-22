e8b69a22e2f91ce1d0e12ba03a26c1eb616f43a4
SiliconSloth <siliconsloth64@gmail.com>
2020-03-22 15:11:11 +0000

    Switch to strncpy for cross-platform support

--- include/gitwrapper/strarray.h
+++ include/gitwrapper/strarray.h
@@ -20,7 +20,7 @@ namespace git {
             char **strings = new char*[list.size()];
             for (int i = 0; i < list.size(); i++) {
                 strings[i] = new char[list[i].size() + 1];
-                strlcpy(strings[i], list[i].c_str(), list[i].size() + 1);
+                strncpy(strings[i], list[i].c_str(), list[i].size() + 1);
             }
             return new git_strarray{strings, list.size()};
         }
